ced30ca49ebef0ffbbf824e52234a6e74263bc1b
Luis Caparroz dos Santos <luis.carlosjr@hotmail.com.br>
2025-02-22 17:35:13 -0300

    Adiciona aula sobre operadores

--- src/01-introducao-c/aula02/01_exemplo_operadores.c
+++ src/01-introducao-c/aula02/01_exemplo_operadores.c
@@ -11,5 +11,23 @@ int main() {
     printf("a >= b: %d\n", a >= b); // Maior ou igual a
     printf("a <= b: %d\n", a <= b); // Menor ou igual a
 
+    float c = 5.0f, d = 10.0f;
+    printf("c = %.2f, d = %.2f\n", c, d);
+    printf("c == d: %d\n", c == d); // Igual a
+    printf("c != d: %d\n", c != d); // Diferente de
+    printf("c > d: %d\n", c > d);   // Maior que
+    printf("c < d: %d\n", c < d);   // Menor que
+
+    char e = 'a', f = 'b';
+    printf("e = %c, f = %c\n", e, f);
+    printf("e == f: %d\n", e == f); // Igual a
+    printf("e != f: %d\n", e != f); // Diferente de
+    printf("e > f: %d\n", e > f);   // Maior que
+
+    char nome[] = "Jose";
+    printf("nome = %s\n", nome);
+    printf("nome == \"Jose\": %d\n", strcmp(nome, "Jose") == 0); // Igual a
+    printf("nome != \"Jose\": %d\n", strcmp(nome, "Jose") != 0); // Diferente de
+
     return 0;
 }
