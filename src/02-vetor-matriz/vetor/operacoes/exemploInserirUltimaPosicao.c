7917f93c5e960c37029189a758b003902d974875
Luis Caparroz <luis.carlosjr@hotmail.com.br>
2025-04-01 15:59:49 -0300

    Adiciona exemplo de inserção de valor na última posição do vetor

--- src/02-vetor-matriz/vetor/operacoes/exemploInserirUltimaPosicao.c
+++ src/02-vetor-matriz/vetor/operacoes/exemploInserirUltimaPosicao.c
@@ -9,3 +9,29 @@ int insereValorVetorNaUltimaPosicao(int vetor[], int tamanho, int valor) {
 
     return 1;
 }
+
+int main() {
+    int vetor[] = {1, 2, 3, 4, 5};
+    int tamanho = sizeof(vetor) / sizeof(vetor[0]);
+    int valor = 6;
+
+    printf("Vetor antes de inserir o valor %d na última posição: ", valor);
+    for (int i = 0; i < tamanho; i++) {
+        printf("%d ", vetor[i]);
+    }
+    printf("\n");
+
+    if (insereValorVetorNaUltimaPosicao(vetor, tamanho, valor)) {
+        tamanho++;
+        printf("Vetor depois de inserir o valor %d na última posição: ", valor);
+        for (int i = 0; i < tamanho; i++) {
+            printf("%d ", vetor[i]);
+        }
+        printf("\n");
+    } else {
+        printf("Não foi possível inserir o valor %d na última posição.\n", valor);
+    }
+
+    return 0;
+}
+// 4: 5, 5: 5
