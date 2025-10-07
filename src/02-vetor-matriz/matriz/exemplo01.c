b78939b000864ae95ee20d0adaa846f086b87a61
Luis Caparroz <luis.carlosjr@hotmail.com.br>
2025-10-07 18:42:31 -0300

    Adicionar funções exemploVetor e exemploMatriz para demonstrar operações com vetores e matrizes

--- src/02-vetor-matriz/matriz/exemplo01.c
+++ src/02-vetor-matriz/matriz/exemplo01.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main() {
+void exemploVetor() {
     float aluno1[4] = {7.5, 8.0, 6.0, 9.0};
     float aluno2[4] = {5.0, 6.5, 7.0, 8.0};
     float aluno3[4] = {9.0, 8.5, 9.5, 10.0};
@@ -8,6 +8,22 @@ int main() {
     // Calcular a média do aluno 2
     float media = (aluno2[0] + aluno2[1] + aluno2[2] + aluno2[3]) / 4;
     printf("Media do aluno 2: %.2f\n", media);
+}
+
+void exemploMatriz() {
+    float notas[3][4] = {
+        {7.5, 8.0, 6.0, 9.0},  // Notas do aluno 1
+        {5.0, 6.5, 7.0, 8.0},  // Notas do aluno 2
+        {9.0, 8.5, 9.5, 10.0}  // Notas do aluno 3
+    };
+
+    // Calcular a média do aluno 2
+    float media = (notas[1][0] + notas[1][1] + notas[1][2] + notas[1][3]) / 4;
+    printf("Media do aluno 2: %.2f\n", media);
+}
+
+int main() {
+
 
     return 0;
 }
